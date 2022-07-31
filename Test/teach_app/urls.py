da42eb2f89ecfd2bdd312bb5e6d8f410288c8732
dmytro.melnyk.it@gmail.com <dmytro.melnyk.it@gmail.com>
2022-07-31 11:07:10 +0400

    refactor code(urls.py). Fix urls

--- Test/teach_app/urls.py
+++ Test/teach_app/urls.py
@@ -4,6 +4,6 @@ from .views import InfoHumanApi
 urlpatterns = [
     path('api/v1/list', InfoHumanApi.as_view()),
     path('api/v1/write', InfoHumanApi.as_view()),
-    path('api/v1/get/<int:pk>', InfoHumanApi.as_view()),
+    path('api/v1/put/<int:pk>', InfoHumanApi.as_view()),
     path('api/v1/delete/<int:pk>', InfoHumanApi.as_view())
 ]
